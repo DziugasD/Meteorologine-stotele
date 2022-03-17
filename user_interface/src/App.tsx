7a5d48bb67a27338854a07399b21b07aa7dc2a71
dapke <dapkeviciusdziugas@outlook.com>
2022-03-17 19:30:28 +0200

    rip

--- user_interface/src/App.tsx
+++ user_interface/src/App.tsx
@@ -1,15 +1,14 @@
-import { useEffect, useState } from 'react';
+import {useLayoutEffect, useEffect, useState } from 'react';
 import axios from 'axios';
-import {DregmeDiv, PapildomaDiv, TemperaturosDiv, VejasDiv, Saule_logo, Lietus_logo, Dregna_logo, Vejas_logo, Naktis_logo } from './Temperaturosdivas'
+import {Telefonu_page, DregmeDiv, PapildomaDiv, TemperaturosDiv, VejasDiv, Saule_logo, Lietus_logo, Dregna_logo, Vejas_logo, Naktis_logo, Main_page } from './Temperaturosdivas'
 
+import {Routes, Route, Navigate } from 'react-router-dom'
 
-function App() {
-  //ORAI
+function App(){
   const [weatherData, setWeatherData] = useState({"ID":7.1500062,"REMOTE_ID":0,"AMBIENT_TEMPERATURE":7.1500062,"GROUND_TEMPERATURE":0,"AIR_QUALITY":0,"AIR_PRESSURE":1400,"HUMIDITY":500,"WIND_DIRECTION":0,"WIND_SPEED":69,"WIND_GUST_SPEED":32,"RAINFALL":0,"CREATED":"1970-01-01T00:00:00.000Z"})
   const [logo, switchlogo] = useState(0)
   var today = new Date();
   var time = today.getHours();
-
 
   function get_latest_data(){
     axios.get('/latest_data').then(function (response) {
@@ -46,113 +45,54 @@ function App() {
       switchlogo(0)
     }
   }
-
-  setInterval(function(){
-    get_latest_data()
-    changeLogo()
-  }, 30000)
-
-
-
-
-  if (time >= 20)
-  {
-    document.body.style.background = "linear-gradient(0deg, #000000, #434343)";
-  }  
-  if (time >= 6 && time < 8)
-  {
-    document.body.style.background = "linear-gradient(0deg, #240b36, #c31432)";
-  }    
-  if (time >= 8 && time < 10)
-  {
-    document.body.style.background = "linear-gradient(0deg, #f12711, #f5af19";
-  }    
-  if (time >= 10 && time < 12)
-  {
-    document.body.style.background = "linear-gradient(0deg, #f4791f, #659999)";
-  }    
-  if (time >= 12 && time < 14)
-  {
-    document.body.style.background = "linear-gradient(0deg, #2980B9, #2980B9, #FFFFFF)";
-  }    
-  if (time >= 14 && time < 16)
-  {
-    document.body.style.background = "linear-gradient(0deg, #7F7FD5, #86A8E7, #91eae4)";
-  }    
-  if (time >= 16 && time < 18)
-  {
-    document.body.style.background = "linear-gradient(0deg, #355C7D, #6C5B7B, #c06c84";
-  }    
-  if (time >= 18 && time < 20)
-  {
-    document.body.style.background = "linear-gradient(0deg, #333333, #dd1818)";
-  }    
-    
-  const [divisionas, keistiDiva] = useState(true)
-  const [divisionas2, keistiDiva2] = useState(true)
-
-  function switchTemp(){
-    keistiDiva(true)
-    keistiDiva2(true)
+  function isMobile(a:number){
+    console.log(a)
+    if (a <700&&window.location.pathname =="/"){
+      window.location.replace("/mobile");   
+    }
+    if(window.location.pathname =="/mobile"&&a >710){
+      window.location.replace("/")
+    }
   }
-  function switchDreg(){
-    keistiDiva(true)
-    keistiDiva2(false)
-  }
-  function switchVejas(){
-    keistiDiva(false)
-    keistiDiva2(true)
-  }
-  function switchLast(){
-    keistiDiva(false)
-    keistiDiva2(false)
-  }
-    
+  
+  const [size, setSize] = useState([705, 1000]);
+  useLayoutEffect(() => {
+    function updateSize() {
+      setSize([window.innerWidth, window.innerHeight]);
+    }
+    window.addEventListener('resize', updateSize);
+    updateSize();
+    return () => window.removeEventListener('resize', updateSize);
+  }, []);
+  
+
   useEffect(() => {
     get_latest_data()
     changeLogo()
   }, []);
 
+  useEffect(() => {
+    isMobile(size[0])
+  }, [size]);
+
+
+  setInterval(function(){
+    get_latest_data()
+  }, 30000)
+
+
   return (
-    <div className='overlay'>
-      <header className='pavadinimas-flex'>
-        <h1 className='pavadinimas'>Ąžuolyno meteorologinė stotelė</h1>
-      </header>
-      <main>
-        <div className='visi-logo'>
-          {
-            logo ===0 && <Saule_logo/>
-          }
-          {
-            logo ===1 &&<Lietus_logo/>
-          }
-          {
-            logo ===2 && <Vejas_logo/>
-          }
-          {
-            logo === 3 && <Naktis_logo/>
-          }
-         </div>
-        <div>
-        {
-          divisionas ? divisionas2? <TemperaturosDiv AMBIENT_TEMPERATURE={weatherData.AMBIENT_TEMPERATURE}/> : <DregmeDiv RAINFALL={weatherData.RAINFALL}/> : divisionas2? <VejasDiv WIND_SPEED={weatherData.WIND_SPEED} WIND_GUST_SPEED= {weatherData.WIND_GUST_SPEED}/> : <PapildomaDiv AIR_PRESSURE={weatherData.AIR_PRESSURE} HUMIDITY={weatherData.HUMIDITY} WIND_GUST_SPEED={weatherData.WIND_GUST_SPEED} />
-        } 
-        </div>
-        
-        
-        <div className='mygtukai-nav'>
-          <button className='temperatura-mygtukas' onClick={switchTemp}>TEMPERATŪRA</button>
-          <button className='vejas-mygtukas' onClick={switchVejas}>VĖJAS</button>  
-          <button className='krituliai-mygtukas' onClick={switchDreg}>KRITULIAI</button>
-          <button className='papildoma-mygtukas' onClick={switchLast}>PAPILDOMA</button>
-        </div>  
-        
-      </main>
-      <footer>
-        <p className='apacioje-tekstas'>D&J 2022</p>
-      </footer>
+    <div >
+      <Routes>
+        <Route path="/" element={Main_page({logo:logo, AMBIENT_TEMPERATURE:weatherData.AMBIENT_TEMPERATURE, WIND_SPEED:weatherData.WIND_SPEED, WIND_GUST_SPEED:weatherData.WIND_GUST_SPEED,  RAINFALL: weatherData.RAINFALL, AIR_PRESSURE: weatherData.AIR_PRESSURE, HUMIDITY:weatherData.HUMIDITY})}/>
+        <Route path="/mobile" element={Telefonu_page({f:logo, AMBIENT_TEMPERATURE:weatherData.AMBIENT_TEMPERATURE, WIND_SPEED:weatherData.WIND_SPEED, WIND_GUST_SPEED:weatherData.WIND_GUST_SPEED,  RAINFALL: weatherData.RAINFALL, AIR_PRESSURE: weatherData.AIR_PRESSURE, HUMIDITY:weatherData.HUMIDITY})}/>
+      </Routes>
+      
     </div>
   );
 }
+
+
+
 
 export default App;
--- user_interface/src/Temperaturosdivas.tsx
+++ user_interface/src/Temperaturosdivas.tsx
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
 
 export function Lietus_logo(){
   return(
@@ -89,4 +92,213 @@ export function PapildomaDiv(weatherData: { AIR_PRESSURE: number; HUMIDITY: numb
       </div>
       </div>
   )
+}
+
+export function Telefonu_page(weatherData:{ f:number; AMBIENT_TEMPERATURE: number; WIND_SPEED: number; WIND_GUST_SPEED: number;  RAINFALL: number; AIR_PRESSURE: number; HUMIDITY: number;} ){
+  //ORAI
+  var today = new Date();
+  var time = today.getHours();
+
+  if (time >= 20)
+  {
+    document.body.style.background = "linear-gradient(0deg, #000000, #434343)";
+  }  
+  if (time >= 6 && time < 8)
+  {
+    document.body.style.background = "linear-gradient(0deg, #240b36, #c31432)";
+  }    
+  if (time >= 8 && time < 10)
+  {
+    document.body.style.background = "linear-gradient(0deg, #f12711, #f5af19";
+  }    
+  if (time >= 10 && time < 12)
+  {
+    document.body.style.background = "linear-gradient(0deg, #f4791f, #659999)";
+  }    
+  if (time >= 12 && time < 14)
+  {
+    document.body.style.background = "linear-gradient(0deg, #2980B9, #2980B9, #FFFFFF)";
+  }    
+  if (time >= 14 && time < 16)
+  {
+    document.body.style.background = "linear-gradient(0deg, #7F7FD5, #86A8E7, #91eae4)";
+  }    
+  if (time >= 16 && time < 18)
+  {
+    document.body.style.background = "linear-gradient(0deg, #355C7D, #6C5B7B, #c06c84";
+  }    
+  if (time >= 18 && time < 20)
+  {
+    document.body.style.background = "linear-gradient(0deg, #333333, #dd1818)";
+  }    
+    
+
+  return (
+    <div className='overlay'>
+      <header className='pavadinimas-flex'>
+        <h1 className='pavadinimas'>Ąžuolyno meteorologinė stotelė</h1>
+      </header>
+      <main>
+         <div>
+            <table className="table-center-css">
+              <tbody>
+                <tr>
+                  <th colSpan={2}>
+                  {
+                    weatherData.f ===0 && <Saule_logo/>
+                  }
+                  {
+                    weatherData.f ===1 &&<Lietus_logo/>
+                  }
+                  {
+                    weatherData.f ===2 && <Vejas_logo/>
+                  }
+                  {
+                    weatherData.f === 3 && <Naktis_logo/>
+                  }
+                  </th>
+                </tr>
+                <tr>
+                  <th>
+                  <p className='tekstas-telefono-informacijai'>TEMPERATŪRA</p>
+                  </th>
+                  <th>
+                    <p className='tekstas-telefono-informacijai'>{(weatherData.AMBIENT_TEMPERATURE).toFixed(1)}°C</p>
+                 </th>
+                </tr>
+                <tr>
+                  <td>
+                    <p className='tekstas-telefono-informacijai'>VĖJO GREITIS</p>
+                  </td>
+                  <td>
+                    <p className='tekstas-telefono-informacijai'>{(weatherData.WIND_SPEED/3.6).toFixed(1)}m/s</p> 
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <p className='tekstas-telefono-informacijai'>KRITULIAI</p>
+                  </td>
+                  <td>
+                    <p className='tekstas-telefono-informacijai'>{(weatherData.RAINFALL).toFixed(1)}mm</p> 
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <p className='tekstas-telefono-informacijai'>ORO SLĖGIS</p>
+                  </td>
+                  <td>
+                   <p className='tekstas-telefono-informacijai'>{(weatherData.AIR_PRESSURE).toFixed(1)}hPa</p> 
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <p className='tekstas-telefono-informacijai'>MAX GREITIS</p>
+                  </td>
+                  <td>
+                    <p className='tekstas-telefono-informacijai'>{(weatherData.WIND_GUST_SPEED/3.6).toFixed(1)}m/s</p> 
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <p className='tekstas-telefono-informacijai'>DREGMĖ</p>
+                  </td>
+                  <td>
+                   <p className='tekstas-telefono-informacijai'>{(weatherData.HUMIDITY).toFixed(0)}%</p> 
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+        </div> 
+      </main>
+      <footer>
+        <p className='apacioje-tekstas'>D&J 2022</p>
+      </footer>
+    </div>
+  );
+}
+
+
+export function Main_page(weatherData:{ logo:number; AMBIENT_TEMPERATURE: number; WIND_SPEED: number; WIND_GUST_SPEED: number;  RAINFALL: number; AIR_PRESSURE: number; HUMIDITY: number;}){
+  //ORAI
+  var today = new Date();
+  var time = today.getHours();
+  if (time >= 20)
+  {
+    document.body.style.background = "linear-gradient(0deg, #000000, #434343)";
+  }  
+  if (time >= 6 && time < 8)
+  {
+    document.body.style.background = "linear-gradient(0deg, #240b36, #c31432)";
+  }    
+  if (time >= 8 && time < 10)
+  {
+    document.body.style.background = "linear-gradient(0deg, #f12711, #f5af19";
+  }    
+  if (time >= 10 && time < 12)
+  {
+    document.body.style.background = "linear-gradient(0deg, #f4791f, #659999)";
+  }    
+  if (time >= 12 && time < 14)
+  {
+    document.body.style.background = "linear-gradient(0deg, #2980B9, #2980B9, #FFFFFF)";
+  }    
+  if (time >= 14 && time < 16)
+  {
+    document.body.style.background = "linear-gradient(0deg, #7F7FD5, #86A8E7, #91eae4)";
+  }    
+  if (time >= 16 && time < 18)
+  {
+    document.body.style.background = "linear-gradient(0deg, #355C7D, #6C5B7B, #c06c84";
+  }    
+  if (time >= 18 && time < 20)
+  {
+    document.body.style.background = "linear-gradient(0deg, #333333, #dd1818)";
+  }    
+    
+  const [divisionas, keistiDiva] = useState(true)
+  const [divisionas2, keistiDiva2] = useState(true)
+
+  function switchTemp(){
+    keistiDiva(true)
+    keistiDiva2(true)
+  }
+  function switchDreg(){
+    keistiDiva(true)
+    keistiDiva2(false)
+  }
+  function switchVejas(){
+    keistiDiva(false)
+    keistiDiva2(true)
+  }
+  function switchLast(){
+    keistiDiva(false)
+    keistiDiva2(false)
+  }
+    
+  return (
+    <div className='overlay'>
+      <header className='pavadinimas-flex'>
+        <h1 className='pavadinimas'>Ąžuolyno meteorologinė stotelė</h1>
+      </header>
+      <main>
+        <div>
+        {
+          divisionas ? divisionas2? <TemperaturosDiv AMBIENT_TEMPERATURE={weatherData.AMBIENT_TEMPERATURE}/> : <DregmeDiv RAINFALL={weatherData.RAINFALL}/> : divisionas2? <VejasDiv WIND_SPEED={weatherData.WIND_SPEED} WIND_GUST_SPEED= {weatherData.WIND_GUST_SPEED}/> : <PapildomaDiv AIR_PRESSURE={weatherData.AIR_PRESSURE} HUMIDITY={weatherData.HUMIDITY} WIND_GUST_SPEED={weatherData.WIND_GUST_SPEED} />
+        } 
+        </div>
+        
+        
+        <div className='mygtukai-nav'>
+          <button className='temperatura-mygtukas' onClick={switchTemp}>TEMPERATŪRA</button>
+          <button className='vejas-mygtukas' onClick={switchVejas}>VĖJAS</button>  
+          <button className='krituliai-mygtukas' onClick={switchDreg}>KRITULIAI</button>
+          <button className='papildoma-mygtukas' onClick={switchLast}>PAPILDOMA</button>
+        </div>  
+        
+      </main>
+      <footer>
+        <p className='apacioje-tekstas'>D&J 2022</p>
+      </footer>
+    </div>
+  );
 }
